0340db3aa4694f930e6efd870a1eca27588502ed
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-09-07 15:44:50 -0700

    added tests still failing one

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,12 @@ function About() {
   return (
     <section className="my-5">
       <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover"></img>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover"/>
+      <div className="my-2">
+        <p>
+          Lorem ipsum hadihadshjasdo asghdsaihas ashgdhiag asgdhagdhakhd ashgfjvfsKjs dahfjsh dshfjsHzfkjhsdKJkj dsjhfsjkdsjkh hdj  dahz hadih hasih  dhjahdfjahj.
+        </p>
+      </div>
     </section>
   );
 }

448033175c3d86aa2dbb3961e84442c41232c7a8
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-09-08 12:42:21 -0700

    added tests but its failing most of them

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -8,6 +8,8 @@ const categories = [
 ];
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected =jest.fn()
+const mockSetContactSelected = jest.fn()
 
 afterEach(cleanup);
 
@@ -19,6 +21,8 @@ describe("Nav component", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        mockContactSelected={mockContactSelected}
+        mockSetContactSelected={mockSetContactSelected}
       />
     );
   });
@@ -30,6 +34,8 @@ describe("Nav component", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        mockContactSelected={mockContactSelected}
+        mockSetContactSelected={mockSetContactSelected}
       />
     );
     expect(asFragment()).toMatchSnapshot();
@@ -44,6 +50,8 @@ describe("emoji is visible", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        mockContactSelected={mockContactSelected}
+        mockSetContactSelected={mockSetContactSelected}
       />
     );
     // Assert
@@ -59,6 +67,8 @@ describe("links are visible", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        mockContactSelected={mockContactSelected}
+        mockSetContactSelected={mockSetContactSelected}
       />
     );
     // Assert
